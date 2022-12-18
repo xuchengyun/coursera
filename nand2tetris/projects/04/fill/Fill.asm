c4a4ff6c3969e534222fbd606d2c2e257d67ec2d
hahagua <454166448@qq.com>
2022-12-17 21:22:20 -0800

    update

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// (LOOP)
+//     @R0
+//     D=M
+//     @R2
+//     M=D+M
+//     @R1
+//     M=M-1
+//     @R1
+//     D=M
+//     @END
+//     D;JLE // infinite loop
+//     @LOOP
+//     @SCREEN
+//     M=1
+// (END) 
+//     @END
+//     0;JMP
+
+
+
+
+(LOOP)
+    @SCREEN
+    D=A
+    @addr
+    M=D
+
+    @8192
+    D=A
+    @n
+    M=D
+
+    @i
+    M=0
+
+    @KBD
+    D=M
+	@BLACKEN
+	D;JNE
+
+(WHITEN)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JGE
+
+    @addr
+    A=M
+    M=0 // RAM[addr]=1111111111111111
+
+    @i
+    M=M+1
+    @1
+    D=A
+    @addr
+    M=D+M
+    @WHITEN
+    0;JMP
+
+(BLACKEN)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JGE
+
+    @addr
+    A=M
+    M=-1 // RAM[addr]=1111111111111111
+
+    @i
+    M=M+1
+    @1
+    D=A
+    @addr
+    M=D+M
+    @BLACKEN
+    0;JMP
+
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,36 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    // @R2
+    // M=0
+    // @R0
+    // D=M
+    // @END
+    // D;JEQ
+    // @R1
+    // D=M
+    // @END
+    // D;JEQ
+
+    @i
+    M=0
+    @R2
+    M=0
+(LOOP)
+    @R1
+    D=M
+    @i
+    D=D-M
+    @END
+    D;JLE // infinite loop
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END) 
+    @END
+    0;JMP
